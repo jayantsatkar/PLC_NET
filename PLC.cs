508fdd048477397100d1198b147ae4059a16617f
Jayant <jayant.satkar@yahoo.in>
2025-09-06 12:58:56 +0530

    Commit 1

---
+++ PLC.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Net.Sockets;
+using Modbus.Data;
+using Modbus.Device; // from NModbus4
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+namespace PLCConnection
+{
+    public class PLC
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+        public static string GetDMCNumber()
+        {
+            Logger.Info("GetDMCNumber");
+            var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+            string plcIp = config.GetSection("PLC")["IpAddress"];//  "10.168.158.230"; // PLC IP address
+            int port = Convert.ToInt16(config.GetSection("PLC")["Port"]);              // Modbus TCP port
+            byte SlaveId = Convert.ToByte(config.GetSection("PLC")["SlaveId"]);
+            //  "SlaveId": 1,
+            //ushort StartAddress = Convert.ushort(config.GetSection("PLC")["StartAddress"]);
+            //"StartAddress": 510,
+            // "RegisterCount": 5
+            
+            string DMC = "";
+
+            try
+            {
+                using (TcpClient client = new TcpClient(plcIp, port))
+                {
+                    // Create Modbus master
+                    var modbusMaster = ModbusIpMaster.CreateIp(client);
+
+                    byte slaveId = SlaveId;  // Unit ID (often 1)
+                    ushort startAddress = 510; // D510
+                    ushort numRegisters = 5;   // D510–D514
+
+                    // Read holding registers
+                    ushort[] registers = modbusMaster.ReadHoldingRegisters(slaveId, startAddress, numRegisters);
+                    byte[] bytes = new byte[registers.Length * 2];
+                    Buffer.BlockCopy(registers, 0, bytes, 0, bytes.Length);
+
+                    for (int i = 0; i < registers.Length; i++)
+                    {
+                        // Swap high/low bytes
+                        bytes[i * 2] = (byte)(registers[i] & 0xFF);      // low
+                        bytes[i * 2 + 1] = (byte)(registers[i] >> 8);         // high
+                    }
+
+                    DMC = Encoding.ASCII.GetString(bytes).TrimEnd('\0');
+
+                    Logger.Info("DMC="+ DMC);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting to PLC: " + ex.Message);
+                Logger.Error(ex);
+            }
+            return DMC;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿
+using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Configuration;
+using PLCConnection;
+using System.Reflection;
+internal class Program
+{
+    private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+            BasicConfigurator.Configure();
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            Logger.Info("Application Started");
+            string FileOkSourcePath = config.GetSection("PLC")["FileOkSourcePath"];//  "10.168.158.230"; // PLC IP address
+
+            if (System.IO.Directory.Exists(FileOkSourcePath))
+            {
+                FileSystemWatcher watcher = new FileSystemWatcher(FileOkSourcePath);
+                watcher.IncludeSubdirectories = true;
+                watcher.Created += FileSystemWatcher_Created_Ok;
+                watcher.EnableRaisingEvents = true;
+            }
+
+            PLC.GetDMCNumber();
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        catch (Exception ex) 
+        { 
+            Logger.Error(ex);
+        }
+    }
+
+    private static void FileSystemWatcher_Created_Ok(object sender, FileSystemEventArgs e)
+    {
+        try
+        {
+            Thread.Sleep(2000);
+            //PLC plc = new PLC();
+            string DMC = PLC.GetDMCNumber();
+
+            string folder = Path.GetDirectoryName(e.FullPath)!;
+            string oldName = Path.GetFileName(e.FullPath);
+            string newName = DMC + "_" + oldName;
+            string newPath = Path.Combine(folder, newName);
+
+            File.Move(e.FullPath, newPath);
+        }
+
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+        }
+        
+    }
+}
+
